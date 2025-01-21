c04a5a1641802d3113e6325b0164294380a06678
Pratap Git <pratapkumarnayak09@gmail.com>
2025-01-21 14:04:39 +0530

    mongo url

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConn = async () => {
-  const conn = await mongoose.connect("mongodb+srv://pratapnayakdev:pratapnayakdev@cluster0.kehl6.mongodb.net/user-roles-jwt");
+  const conn = await mongoose.connect("mongodb+srv://pratapnayakdev:<password>@cluster0.kehl6.mongodb.net/user-roles-jwt");
   try {
     if (conn) {
       console.log("DB Connected");
